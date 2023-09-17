b52e226b99cefa275524dd9fb63dd00e769b5640
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-17 19:59:09 +0300

    add task1 and task2

---
+++ IOS'nizza_tasks.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// MARK: - Basic operators
+// Высчитайте площадь прямоугольника, зная ешо длину и ширину
+if true {
+    let length = 12
+    let width = 10
+    
+    let square = length * width
+    print("1. square = \(square)")
+}
+
+// Высчитайте итоговую сумм оплаты, зная сумму за покупки и процент скидки. Сколько покупателю ннужно оплатить?
+if true {
+    let sum: Double = 123.6
+    let discount: Double = 12.0
+    
+    let result = sum * (100 - discount) / 100
+    print("2. result = \(result)")
+}
