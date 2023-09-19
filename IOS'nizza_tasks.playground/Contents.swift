40139bfc25e20ec7fdafcb0ac4c7ee18d5c93dcd
Balandzin_Anton <balandzin2602@gmail.com>
2023-09-19 20:25:39 +0300

    add task 6 and tas 7

--- IOS'nizza_tasks.playground/Contents.swift
+++ IOS'nizza_tasks.playground/Contents.swift
@@ -6,22 +6,26 @@ if true {
     let length = 12
     let width = 10
     
-    let square = length * width
+    func getSquare(length: Int, width: Int) -> Int {
+        length * width
+    }
+    
+    let square = getSquare(length: length, width: width)
     print("1. square = \(square)")
 }
 
 // Высчитайте итоговую сумм оплаты, зная сумму за покупки и процент скидки. Сколько покупателю ннужно оплатить?
 if true {
     let sum: Double = 123.6
-    let discount: Double = 12.0
+    let discont: Double = 12.0
     
-    let result = sum * (100 - discount) / 100
+    let result = sum * (100 - discont) / 100
     print("2. result = \(result)")
 }
 
 // Напишите программу, котрая будет определять является ли число четным или не четным
 if true {
-    let number = 12
+    let number = 13
     
     var result = number % 2 == 0 ? "Четное" : "Нечетное"
     print("3. result = \(result)")
@@ -32,12 +36,11 @@ if true {
 if true {
     var str1 = "!ьнед йынсаркерп йокаК"
     
-    var result = Array(str1.reversed())
-    print("4. \(String(result))")
+    var result = String(str1.reversed())
+    print("4. \(result)")
 }
 
-// Посчитайте количество гласных в строке
-
+// Посчитайте количество гласных в  произзвольной строке
 if true {
     let array: [Character] = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я" ]
     let str = "Какой прекрасный день"
@@ -47,11 +50,38 @@ if true {
     for i in str.lowercased() {
         arrayChar.append(i)
     }
-
+    
     for element in arrayChar {
         if array.contains(element){
             count += 1
         }
     }
     print("5. count = \(count)")
+}
+
+// Создайте несколько констант типа String с произвольным значением. Определите, какая константа содержит самую длинную строку
+if true {
+    
+    let str1: String = "My name is Anton"
+    let str2: String = "I'm 33"
+    let str3: String = "I am from Belarus"
+    
+    if str1.count > str2.count && str1.count > str3.count {
+        print("6. \(str1) - the biggest")
+    } else if str2.count > str1.count && str2.count > str3.count {
+        print("6. \(str2) - the biggest")
+    } else if str3.count > str2.count && str3.count > str1.count {
+        print("6. \(str3) - the biggest")
+    }
+}
+
+// MARK: - Collections
+// В произвольном массиве типа Int найдите наибольший элемент
+if true {
+    let array: [Int] = [2, 42, -47, 0, 45678]
+    
+    let maxElement = array.max()
+    print("7. max = \(maxElement!)")
+    
+    
 }
